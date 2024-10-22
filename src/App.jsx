04aa8338d9e7470051949621f08703bd03004700
Adil Isani <adil.isani1@gmail.com>
2024-10-22 23:08:30 +0500

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+      </Router>
+    </>
+
+  );
+}
+
+export default App;
